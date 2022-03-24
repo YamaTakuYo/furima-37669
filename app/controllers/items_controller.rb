22ca74164d13e9b5d4082935ebe5124ff4cb351c
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-24 17:49:56 +0900

    商品出品へのpath

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :destroy]
+    
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user, :name, :price, :description, :category_id, :condition_id,  :prefecture_id, :delivery_cost_id, :derivery_dete_id)
+  end
+
 end
