c8db20fe82711dd577fbdda86ac789ecae651327
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-24 17:06:18 +0900

    Itemバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :user
+    validates :name
+    validates :price
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :prefecture_id
+    validates :delivery_cost_id
+    validates :delivery_date_id
+
 end
