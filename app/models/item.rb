b383f59bcc706fa1f4aaa69a1eb670250d4c22ea
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-24 19:20:22 +0900

    ActiveHash::Base＆itemsモデル　バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :delivery_cost
+  belongs_to :delivery_date
 
   with_options presence: true do
     validates :user
@@ -13,5 +19,15 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :delivery_cost_id
     validates :delivery_date_id
+  end
+
+  with_options numericality: { other_than: 1 ,message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :prefecture_id
+    validates :delivery_cost_id
+    validates :delivery_date_id
+  end
+
 
 end
