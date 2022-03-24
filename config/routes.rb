2ffbfef8a7321f96b76292dbc6eed414021161e2
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-24 17:01:37 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
+
+  resource :items do
+  end
 end
