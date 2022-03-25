978246fa638beb543f95ac2b6056f68bdfb30cbf
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-25 12:21:39 +0900

    価格のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,6 +21,8 @@ class Item < ApplicationRecord
     validates :delivery_date_id
     validates :image
   end
+  
+  validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
