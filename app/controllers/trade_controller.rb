d9a7aa0e09fc6cec4485a2b67250c3d7b865e6b9
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-29 11:29:37 +0900

    tradeコントローラーの修正

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -7,6 +7,11 @@ class TradeController < ApplicationController
     @trade = Trade.new(trade_params)
   end
 
+  private
 
+  def trade_params
+    params.require(:trade).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id])
+  end
 
 end
