062984ff08957e5b680a697f2c70bcf9e704a2ea
YamaTakuYo <yamatakuyo@users.noreply.github.com>
2022-03-30 13:43:34 +0900

    formオブジェクトの追記、バリデーションの追記

--- app/models/trade_buyer.rb
+++ app/models/trade_buyer.rb
@@ -13,7 +13,7 @@ class TradeBuyer
     validates :token
   end
 
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択して下さい'}
 
   def save
     trade = Trade.create(user_id: user_id, item_id: item_id)
